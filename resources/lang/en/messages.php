6697f7573911f0276b38cc1f71f5d00775085370
Lukas Mateffy <github@mateffy.me>
2024-02-06 17:45:06 +0100

    wip

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,11 +2,15 @@
 
 return [
     'cannot-download-folder' => 'You can not download a folder',
+    'no-files-uploaded' => 'No files were uploaded',
     'no-preview-available' => 'No preview available',
     'no-files-selected' => 'No file selected|No files selected',
     'cannot-select-file' => 'File could not be selected',
     'not-eligble-for-artifact-regeneration' => 'File type does not support artifact regeneration',
     'artefact-regeneration-confirmed' => 'Artifact regeneration queued',
     'artefact-file-count' => 'One file affected|:count files affected',
+    'invalid-file-types' => 'This file\'s type is not allowed|:count file types are not allowed',
+    'files-uploaded-successfully' => 'One file uploaded successfully|:count files uploaded successfully',
+    'files-skipped' => 'One file was skipped|:count files were skipped',
     'unknown-error' => 'An unknown error occurred. Please try again.',
 ];
